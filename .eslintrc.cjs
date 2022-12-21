092f7863cf777ed6e3a3129b4ed4df28d94ca2fd
ModyQyW <wurui-dev@foxmail.com>
2022-12-21 10:39:50 +0800

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   root: true,
-  extends: [require.resolve('@modyqyw/fabric/eslint/vanilla')],
-  overrides: [
-    {
-      files: ['*.ts', '**/*.ts'],
-      extends: [require.resolve('@modyqyw/fabric/eslint/typescript')],
-    },
+  extends: [
+    require.resolve('@modyqyw/fabric/eslint/vanilla'),
+    require.resolve('@modyqyw/fabric/eslint/typescript'),
   ],
 };
