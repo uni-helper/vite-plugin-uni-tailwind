cdb1d422b504ddae83f5ffb0821ea4a8dd150376
ModyQyW <wurui-dev@foxmail.com>
2023-09-20 18:15:08 +0800

    chore: split replaceElements

--- src/style/postcss.ts
+++ src/style/postcss.ts
@@ -1,53 +1,6 @@
 import { Plugin, Rule } from 'postcss';
-import { replaceCharacters } from '../utils';
+import { replaceCharacters, replaceElements } from '../utils';
 import { defaultOptions } from '../options';
-
-const postcssReplaceElements = (selector: string, options = defaultOptions) => {
-  let newSelector = selector;
-
-  const { spaceBetweenElements, divideWidthElements, elementMap } = options;
-  newSelector = newSelector
-    .replace(
-      /^\.(-?space-\w)(-.+?)\s?>.*/,
-      spaceBetweenElements
-        .map((element) => `.$1$2>${element}:not([hidden]):not(:first-child)`)
-        .join(','),
-    )
-    .replace(
-      /^\.(-?space-\w-reverse).*/,
-      spaceBetweenElements
-        .map((element) => `.$1>${element}:not([hidden]):not(:first-child)`)
-        .join(','),
-    )
-    // divide * https://tailwindcss.com/docs/divide-width
-    .replace(
-      /^\.(-?divide-\w+)(-.+?)?\s?>.*/,
-      divideWidthElements
-        .map((element) => `.$1$2>${element}:not([hidden]):not(:first-child)`)
-        .join(','),
-    )
-    .replace(
-      /^\.(-?divide-\w-reverse).*/,
-      divideWidthElements
-        .map((element) => `.$1>${element}:not([hidden]):not(:first-child)`)
-        .join(','),
-    );
-
-  for (const [key, value] of elementMap) {
-    newSelector = newSelector.replace(
-      new RegExp(
-        key === '*'
-          ? // eslint-disable-next-line no-useless-escape
-            `(?<![a-zA-Z-> ])\\${key}(?=[\,\s\0{])|(?<![a-zA-Z-> ])\\${key}$`
-          : // eslint-disable-next-line no-useless-escape
-            `(?<![a-zA-Z-> ])${key}(?=[\,\s\0{])|(?<![a-zA-Z-> ])${key}$`,
-      ),
-      value.join(','),
-    );
-  }
-
-  return newSelector;
-};
 
 export function postcssTransformSelector(options = defaultOptions): Plugin {
   return {
--- src/utils.ts
+++ src/utils.ts
@@ -15,3 +15,50 @@ export const replaceCharacters = (
   }
   return newSource;
 };
+
+export const replaceElements = (source: string, options = defaultOptions) => {
+  let newSource = source;
+
+  const { spaceBetweenElements, divideWidthElements, elementMap } = options;
+  newSource = newSource
+    .replace(
+      /^\.(-?space-\w)(-.+?)\s?>.*/,
+      spaceBetweenElements
+        .map((element) => `.$1$2>${element}:not([hidden]):not(:first-child)`)
+        .join(','),
+    )
+    .replace(
+      /^\.(-?space-\w-reverse).*/,
+      spaceBetweenElements
+        .map((element) => `.$1>${element}:not([hidden]):not(:first-child)`)
+        .join(','),
+    )
+    // divide * https://tailwindcss.com/docs/divide-width
+    .replace(
+      /^\.(-?divide-\w+)(-.+?)?\s?>.*/,
+      divideWidthElements
+        .map((element) => `.$1$2>${element}:not([hidden]):not(:first-child)`)
+        .join(','),
+    )
+    .replace(
+      /^\.(-?divide-\w-reverse).*/,
+      divideWidthElements
+        .map((element) => `.$1>${element}:not([hidden]):not(:first-child)`)
+        .join(','),
+    );
+
+  for (const [key, value] of elementMap) {
+    newSource = newSource.replace(
+      new RegExp(
+        key === '*'
+          ? // eslint-disable-next-line no-useless-escape
+            `(?<![a-zA-Z-> ])\\${key}(?=[\,\s\0{])|(?<![a-zA-Z-> ])\\${key}$`
+          : // eslint-disable-next-line no-useless-escape
+            `(?<![a-zA-Z-> ])${key}(?=[\,\s\0{])|(?<![a-zA-Z-> ])${key}$`,
+      ),
+      value.join(','),
+    );
+  }
+
+  return newSource;
+};
