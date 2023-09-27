120cb0b71d9544fe8cdab32d47016fcad694f81a
ModyQyW <wurui-dev@foxmail.com>
2023-09-27 17:12:30 +0800

    Revert "feat:  remove babel and replace template characters directly"
    This reverts commit 48dcd32e9d71fefe5306ee893609a932e232d9a0.

--- src/template/index.ts
+++ src/template/index.ts
@@ -1,6 +1,6 @@
 import * as wxml from '@vivaxy/wxml';
 import { defaultOptions } from '../options';
-import { replaceCharacters } from '../utils';
+import { babelTransformClass } from './babel';
 
 export const isTemplateFile = (fileName: string) => /.+\.(?:wx|ax|jx|ks|tt|q)ml$/.test(fileName);
 
@@ -12,9 +12,11 @@ export const transformTemplate = (source: string, options = defaultOptions) => {
         options.shouldTransformTemplateAttribute(k),
       );
       for (const k of keys) {
-        node.attributes[k] = replaceCharacters(node.attributes[k], 'template', options);
+        node.attributes[k] = babelTransformClass(node.attributes[k], options);
       }
     }
   });
   return wxml.serialize(parsed);
 };
+
+export { babelTransformClass } from './babel';
